e5f90a1f52dde62759bc56a435c7069557a5f917
Giulia Ivan <89975187+GiuliaIvan@users.noreply.github.com>
2025-05-19 13:32:55 +0200

    Revert "Merge branch 'Giulia6'"
    This reverts commit daaa2d843da5b4bf4b946a019112fe96b0804abe, reversing
    changes made to 4e70b3f538263745e2aa48ccbecfaab19458ff7c.

--- FragmentsOfTime/Assets/Scripts/ShadowCaster2DFromComposite.cs
+++ FragmentsOfTime/Assets/Scripts/ShadowCaster2DFromComposite.cs
@@ -11,10 +11,8 @@ public class ShadowCaster2DFromComposite : MonoBehaviour
 {
     void Start()
     {
-        if (!Application.isPlaying) return;
         GenerateShadowCasters();
     }
-
 
     void GenerateShadowCasters()
     {
@@ -48,8 +46,6 @@ public class ShadowCaster2DFromComposite : MonoBehaviour
             GameObject caster = new GameObject("ShadowCaster2D");
             caster.transform.SetParent(transform, false);
 
-            caster.layer = LayerMask.NameToLayer("ShadowCasterLayer");
-
             var shadowCaster = caster.AddComponent<ShadowCaster2D>();
             var poly = caster.AddComponent<PolygonCollider2D>();
             poly.points = path;
@@ -69,5 +65,7 @@ public class ShadowCaster2DFromComposite : MonoBehaviour
             so.ApplyModifiedProperties();
 #endif
         }
+
+        Debug.Log("Shadow casters created.");
     }
 }
