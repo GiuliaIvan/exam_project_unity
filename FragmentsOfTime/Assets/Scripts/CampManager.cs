75e3d6b1cfc6a22b2bc73fa8539a4bbea3027532
lilidkelemen <eaalike@students.eaaa.dk>
2025-05-22 12:53:01 +0200

    menu overlay + btn sfx

--- FragmentsOfTime/Assets/Scripts/CampManager.cs
+++ FragmentsOfTime/Assets/Scripts/CampManager.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public class CampManager : MonoBehaviour
 {
+    public GameObject menuOverlay;
 
+    public void ShowMenu()
+    {
+        menuOverlay.SetActive(true);
+    }
+
+    public void HideMenu()
+    {
+        menuOverlay.SetActive(false);
+    }
 
 }
